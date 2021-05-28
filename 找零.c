4001f88524858d0e924483e498d352364d64a45a
clh1751764916 <57035004+clh1751764916@users.noreply.github.com>
2021-05-28 19:00:20 +0800

    Add files via upload

---
+++ 找零.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int price=0;
+    printf("ÇëÊäÈë½ğ¶î");
+    scanf("%d",&price);
+
+    int change=100-price;
+
+    printf("ÕÒÄã%dÔª",change);
+
+    return 0;
+}
